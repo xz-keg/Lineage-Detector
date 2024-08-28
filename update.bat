a46305361381689caa4809beacab29814ddffac1
Yat-sing Ng <38100637+Over-There-Is@users.noreply.github.com>
2024-08-28 16:21:17 +0800

    Add files via upload
    attach "-out" after the output filename

---
+++ update.bat
@@ -0,0 +1,3 @@
+wget -O des.json https://nextstrain.org/charon/getDataset?prefix=staging/nextclade/sars-cov-2/
+wget -O analyse.py https://raw.githubusercontent.com/aviczhl2/usher_analyse_tool/main/analyse.py
+pause
