4cd8da3db1f4e825137aacbcd8c4d401f9585b92
aviczhl2 <zoux18@mails.tsinghua.edu.cn>
2024-10-26 15:50:15 +0800

    a

--- designation_count.py
+++ designation_count.py
@@ -1,7 +1,17 @@
 ycount={}
 import json
 import copy
-def designation_browser(current_node,current_mut):
+def read_ref():
+    ref=open("reference_seq.txt",'r')
+    q=ref.readlines()
+    seq=''
+    for l in q:
+        for w in l:
+            if w in ['a','t','c','g']:
+                seq=seq+w
+    return seq.upper()
+
+def designation_browser(current_node,current_mut,anno,current_ref):
     mut=[]
     if 'branch_attrs' in current_node:
         #print(current_node['branch_attrs'])
@@ -10,6 +20,7 @@ def designation_browser(current_node,current_mut):
     all_mutations=copy.deepcopy(current_mut)
     for item in mut:
         nuc_pos=int(item[1:-1])
+        current_ref[:nuc_pos]=
         already=False
         for item2 in current_mut:
             if int(item2[1:-1])==nuc_pos:
@@ -33,15 +44,28 @@ def designation_browser(current_node,current_mut):
             ycount[yy]+=1
     if 'children' in current_node:
         for child in current_node['children']:
-            w=designation_browser(child,all_mutations)
+            w=designation_browser(child,all_mutations,anno,current_ref)
 
     return 0
 
 def read_designation():
     w=open("des.json",'r')
-    q=json.load(w)
+    js=json.load(w)
     w.close()
-    sp=designation_browser(q['tree'],[])
+    anno=js['meta']['genome_annotations']
+    js['meta']['colorings'][0]['scale'].append(['highlighted sample','#CCCC00'])
+
+    anno['ORF1a']=anno['ORF1ab']['segments'][0]
+    anno['ORF1b']=anno['ORF1ab']['segments'][1]
+    anno['ORF9b']={'start':28284,'end':28577}
+    anno['ORF9c']={'start':28734,'end':28955}
+    anno['ORF3c']={'start':25457,'end':25582}
+    anno['ORF3b']={'start':25814,'end':25882}
+    anno['ORF3d']={'start':25524,'end':25697}
+    anno['ORF3d-2']={'start':25968,'end':26069}
+    anno['ORF0']={'start':107,'end':136}
+    ref=read_ref()
+    sp=designation_browser(js['tree'],[],anno,ref)
     return 0
 read_designation() 
 print(ycount)
